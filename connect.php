73c951e6f365eab41db64aceb97c544211eaa5a6
datprovip123 <thanhdat8401@gmail.com>
2021-06-16 10:34:32 +0700

    datvip

--- connect.php
+++ connect.php
@@ -1,6 +1,12 @@
 <?php
-$connect = mysqli_connect('localhost','root','','web_music');
-if (!$connect){
-echo "ket noi that bai";
+//$connect = mysqli_connect('localhost','root','','web_music');
+//if (!$connect){
+//echo "ket noi that bai";
+
+$server = "52.6.114.59";
+$username = "datmusic";
+$password = "datmusic";
+$dbname ="datprovip";
+$connect = mysqli_connect($server, $username, $password, $dbname);
 }
 ?>
